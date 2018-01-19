ae767023568ff5d9817f2b0ec893da232e766f14
Philipp Melab <philipp.melab@gmail.com>
2018-01-19 18:39:21 +0100

    Library fixes.

--- assets/debug.js
+++ assets/debug.js
@@ -4,7 +4,7 @@
       $('.graphql-twig-debug-wrapper', context).once('graphql-debug').each(function () {
         var query = $(this).attr('data-query');
         var variables = $(this).attr('data-variables');
-        var $form = $('<form action="/graphql/explorer" method="post" target="_blank"></form>').appendTo(this);
+        var $form = $('<form class="graphql-twig-debug-form" action="/graphql/explorer" method="post" target="_blank"></form>').appendTo(this);
         $('<input type="hidden" name="query"/>').val(query).appendTo($form);
         $('<input type="hidden" name="variables"/>').val(variables).appendTo($form);
         $('<input type="submit" value="Inspect GraphQL query"/>').appendTo($form);
