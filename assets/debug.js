09e0291146769c449f0037f51f7be3d8c4f7db03
Philipp Melab <philipp.melab@gmail.com>
2018-05-17 16:16:28 +0200

    JS fix.

--- assets/debug.js
+++ assets/debug.js
@@ -2,8 +2,8 @@
   Drupal.behaviors.graphqlTwigDebug = {
     attach: function (context, settings) {
       $('.graphql-twig-debug-wrapper', context).once('graphql-debug').each(function () {
-        var query = $(this).attr('data-query');
-        var variables = $(this).attr('data-variables');
+        var query = $(this).attr('data-graphql-query');
+        var variables = $(this).attr('data-graphql-variables');
         var $form = $('<form action="/graphql/explorer" method="post" target="_blank"></form>').appendTo(this);
         $('<input type="hidden" name="query"/>').val(query).appendTo($form);
         $('<input type="hidden" name="variables"/>').val(variables).appendTo($form);
