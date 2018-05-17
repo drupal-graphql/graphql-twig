4b08f9cad7de2edb91da77865530a148cf13ac96
Philipp Melab <philipp.melab@gmail.com>
2018-05-17 16:24:00 +0200

    JS fix.

--- assets/debug.js
+++ assets/debug.js
@@ -5,6 +5,9 @@
         var query = $(this).attr('data-graphql-query');
         var variables = $(this).attr('data-graphql-variables');
         var $form = $('<form action="/graphql/explorer" method="post" target="_blank"></form>').appendTo(this);
+        var txt = document.createElement('textarea');
+        txt.innerHTML = variables;
+        variables = txt.value;
         $('<input type="hidden" name="query"/>').val(query).appendTo($form);
         $('<input type="hidden" name="variables"/>').val(variables).appendTo($form);
         $('<input type="submit" class="graphql-twig-debug-button" value="Inspect GraphQL query"/>').appendTo($form);
