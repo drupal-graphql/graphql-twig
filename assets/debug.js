5159f7177fd37f15b23ce76dc32f24b990ca0c65
Olli Vesslin <olli7@users.noreply.github.com>
2019-10-07 11:16:44 +0300

    Use Drupal.url() to fix form action. (#17)

--- assets/debug.js
+++ assets/debug.js
@@ -4,7 +4,7 @@
       $('.graphql-twig-debug-wrapper', context).once('graphql-debug').each(function () {
         var query = $(this).attr('data-graphql-query');
         var variables = $(this).attr('data-graphql-variables');
-        var $form = $('<form action="/graphql/explorer" method="post" target="_blank"></form>').appendTo(this);
+        var $form = $('<form method="post" target="_blank"></form>').attr('action', Drupal.url('graphql/explorer')).appendTo(this);
         var txt = document.createElement('textarea');
         txt.innerHTML = variables;
         variables = txt.value;
